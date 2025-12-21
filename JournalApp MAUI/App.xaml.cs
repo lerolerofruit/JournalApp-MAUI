1fec6e6cd2bef08dcc0e720f873223b905b77892
kaush <kaush@DESKTOP-9H9TUQ7>
2025-12-21 15:10:05 +0545

    Add project files.

---
+++ JournalApp MAUI/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace JournalApp_MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "JournalApp MAUI" };
+        }
+    }
+}
